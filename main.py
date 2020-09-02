ce486f09297768b3eaa97f55f3b85798fb42dd89
Can Kırşallıoba <cankirsallioba@gmail.com>
2020-09-02 17:29:51 +0300

    Added more stuff

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import os
 import time
 import random
 
+WIDTH, HEIGHT = 750, 750
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Invaders by CK")
+
 #Load enemy ships
 RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
 BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
@@ -12,11 +16,25 @@ GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_sm
 YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
 
 #Load lasers
-RED_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red"))
-YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow"))
-BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue"))
-GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green"))
+RED_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
+YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
+BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
+GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
 
 #Load background
 BG = pygame.image.load(os.path.join("assets", "background-black.png"))
 
+def main():
+    run = True
+    FPS = 60
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+
+main()
